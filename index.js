09059689fade064a00276e40ce21f4965dbd194a
ahzia <ah.ziayosfi@gmail.com>
2022-04-01 01:40:38 +0430

    fix port issue for heroku deployment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/hotels', hotelsRoute);
 app.use('/services', servicesRoute);
 app.use('/guests', guestsRoute);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(
   process.env.DB_CONNECTION,
   () => console.log('DB connected'),
